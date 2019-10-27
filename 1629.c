0e241f0d99b4fa67fcbeb7d6919bb9f6c93eaf51
Moreu-j <56477487+Moreu-j@users.noreply.github.com>
2019-10-28 01:12:21 +0900

    19-10-28 solved

--- 1629.c
+++ 1629.c
@@ -1,35 +1,41 @@
 #include <stdio.h>
-#include <math.h>
-int my_pow(int A, int B, int C);
+
+long my_pow(long A, long B, long C);
 
 int main(){
-	int A,B,C;
-	int cul;
-	int i;
+	long A,B,C;
+	long cul;
 
 	scanf("%d %d %d",&A,&B,&C);		
-	cul=my_pow(A%C,B,C);
+
+	cul=my_pow(A,B,C);
 
 	printf("%d\n",cul);
 
 	return 0;
 }
 
-int my_pow(int A, int B, int C)
-{
-	int tmp;
+long my_pow(long A, long B, long C){
+	long tmp;
 
-	if(B == 1)
-		return A;
+	if(B==0) //더이상 나눌 횟수가 없다
+		return 1;
 
-	else
+	tmp = my_pow(A,B/2,C);
+	tmp = tmp*tmp % C;
+/*
+	if(B%2==0) //짝수면 패스
+		return tmp;
+
+	else //홀수면 한번 마지막한번 곱해줌
 	{
-		tmp=my_pow(A,B/2,C);
-		if(B%2)
-			return ((tmp*tmp) % C * A) % C;
-		else
-			return (tmp*tmp) % C;
+		return tmp*A %C;
+	}
+*/
+	if(B%2==1) //홀수면 한번 더 계산
+	{
+		return tmp*A %C;
 	}
 
-	return 0;
+	return tmp;
 }
